5d5dd6ee7947c3c0fc56bdd0fdb9ba751d76d890
DinhHieu2506 <hieucr78@gmail.com>
2025-06-12 15:11:44 +0700

    Remove Vercel configuration and simplify Vite input paths

--- movie-app/vite.config.js
+++ movie-app/vite.config.js
@@ -1,14 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        detail: resolve(__dirname, 'detail.html'),
+        main: 'index.html',
+        detail: 'detail.html'
       }
     }
   }
 })
+
